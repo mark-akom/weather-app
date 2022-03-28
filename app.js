ae8cb046923b0d35a7e7db5559d0cf5508353794
mark-akom <mark.akom2019@gmail.com>
2022-03-28 21:09:53 +0000

    Added most of the html content to the index.html file, completed most of the styling and almost done with the JS code.

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
+const searchButton = document.querySelector('.search-btn');
+const searchInput = document.querySelector('.city-search');
+const place = document.querySelector('.place');
+const iconImg = document.querySelector('.icon-img');
+const weatherDesc = document.querySelector('.temp-desc')
+const tempValue = document.querySelector('.temp-value');
+const humidityValue = document.querySelector('.hum-value');
+const windValue = document.querySelector('.wind-value');
+const feelsValue = document.querySelector('.feel-value');
 
 const weather = (() => {
     const _API_KEY = `6e58b9a04c5a391d7fd4da6259a02c25`;
-    let userCity = 'Abuja';
+    let userCity = 'Accra';
     const getCity = (city) => {
         userCity = city;
     }
 
-    const processForecast = ({weather, main, wind}) => {
+    const processForecast = ({weather, main, wind, sys, name}) => {
         const { description, icon } = weather[0];
-        const { humidity, temp, } = main
+        const { humidity, temp, feels_like } = main
+        const { country } = sys;
         const { speed } = wind
-        console.log(wind);
+        const weatherData = Object.assign({}, {description, icon, humidity, temp, feels_like, speed, country, name})
+        updateDOM(weatherData);
     }
 
     const getCurrentForecast =  async () => {
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${userCity}&APPID=${_API_KEY}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        processForecast(data);
+
+        try {
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${userCity}&APPID=${_API_KEY}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            processForecast(data);
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    const updateDOM = (weatherData) => {
+        place.textContent = `${weatherData.name} - ${weatherData.country}`;
+        iconImg.src = `http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
+        weatherDesc.textContent = weatherData.description;
+        tempValue.textContent = weatherData.temp;
+        humidityValue.textContent = weatherData.humidity;
+        windValue.textContent = weatherData.speed;
+        feelsValue.textContent = weatherData.feels_like
     }
 
     return {
@@ -26,6 +54,16 @@ const weather = (() => {
     }
 })();
 
-// weather.getCity('Kumasi');
 
-// weather.getCurrentForecast();
+searchButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const userCity = searchInput.value.trim();
+
+    if (userCity.length > 0) {
+        // console.log(userCity);
+        weather.getCity(userCity);
+        weather.getCurrentForecast();
+        searchInput.value = '';
+    }
+})
+
