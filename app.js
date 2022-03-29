6032be52dbcbef7a095ca8bc6ecd9b11553a6ad6
mark-akom <mark.akom2019@gmail.com>
2022-03-29 11:01:47 +0000

    Updated the weather values to their SI formats and also added a toggle button to switch units. Added styles for the toggle button. Added code to make the toggle button work and code for change temp units.

--- app.js
+++ app.js
@@ -7,28 +7,34 @@ const tempValue = document.querySelector('.temp-value');
 const humidityValue = document.querySelector('.hum-value');
 const windValue = document.querySelector('.wind-value');
 const feelsValue = document.querySelector('.feel-value');
+const tempToggle = document.querySelector('.temp-toggle');
+
 
 const weather = (() => {
     const _API_KEY = `6e58b9a04c5a391d7fd4da6259a02c25`;
     let userCity = 'Accra';
+    let processedData = {};
     const getCity = (city) => {
         userCity = city;
     }
 
     const processForecast = ({weather, main, wind, sys, name}) => {
         const { description, icon } = weather[0];
-        const { humidity, temp, feels_like } = main
+        let { humidity, temp, feels_like } = main
         const { country } = sys;
-        const { speed } = wind
+        const { speed } = wind;
+        temp = parseFloat((temp - 273.15).toFixed(2));
+        feels_like = parseFloat((feels_like - 273.15).toFixed(2));
         const weatherData = Object.assign({}, {description, icon, humidity, temp, feels_like, speed, country, name})
         updateDOM(weatherData);
+        processedData = Object.assign({}, weatherData);
     }
 
     const getCurrentForecast =  async () => {
 
         try {
             const url = `http://api.openweathermap.org/data/2.5/weather?q=${userCity}&APPID=${_API_KEY}`;
-            const response = await fetch(url);
+            const response = await fetch(url, {mode: 'cors'});
             const data = await response.json();
             console.log(data);
             processForecast(data);
@@ -42,17 +48,47 @@ const weather = (() => {
         place.textContent = `${weatherData.name} - ${weatherData.country}`;
         iconImg.src = `http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
         weatherDesc.textContent = weatherData.description;
-        tempValue.textContent = weatherData.temp;
-        humidityValue.textContent = weatherData.humidity;
-        windValue.textContent = weatherData.speed;
-        feelsValue.textContent = weatherData.feels_like
+        tempValue.innerHTML = `${weatherData.temp} &#8451;`;
+        humidityValue.textContent = `${weatherData.humidity}%`;
+        windValue.textContent = `${weatherData.speed}m/s`;
+        feelsValue.innerHTML = ` ${weatherData.feels_like} &#8451;`;
+    }
+
+    const returnProcessedData = () => {
+        return processedData
     }
 
     return {
         getCurrentForecast,
-        getCity
+        getCity,
+        returnProcessedData,
     }
 })();
+
+function convertTempToCelsius({feels_like, temp}) {
+    tempValue.innerHTML = `${temp.toFixed(2)} &#8451;`
+    feelsValue.innerHTML = ` ${feels_like.toFixed(2)} &#8451;`
+}
+
+function convertTempToFahrenheit({feels_like, temp}) {
+    let currentTemp = (temp * (9 / 5)) + 32;
+    let currentFeels = (feels_like * (9 / 5)) + 32;
+    tempValue.innerHTML = `${currentTemp.toFixed(2)} &#8457;`
+    feelsValue.innerHTML = ` ${currentFeels.toFixed(2)} &#8457;`
+}
+
+tempToggle.addEventListener('click', () => {
+    const span = tempToggle.firstElementChild
+    if(span.className === 'F') {
+        span.className = 'C';
+        span.innerHTML = '&#8451;'
+        convertTempToFahrenheit(weather.returnProcessedData())
+    } else {
+        span.className = 'F'
+        span.innerHTML = '&#8457;'
+        convertTempToCelsius(weather.returnProcessedData());
+    }
+})
 
 
 searchButton.addEventListener('click', (e) => {
